7ac096239c158fef97787c37da8a4cbc797c0b60
koudaiii <cs006061@gmail.com>
2015-03-16 22:49:23 +0900

    add dialog

--- kimataku-genkibot.rb
+++ kimataku-genkibot.rb
@@ -37,41 +37,45 @@ stream.on_timeline_status do |status|
   log.info('timeline: %s' % ["#{Time.now}"])
   log.info('@%s said : %s' % [status.user.screen_name, status.text])
   next if status.retweet?
-  next if status.reply?
-
-  shinpai = '@%s ' % status.user.screen_name
-  case status.text
-  when /https?:\/\//
-    next
-  when /疲(?!れ(?:様|さ(?:ま|ん)))/
-    shinpai += '疲れてるの？'
-  when /凹/
-    shinpai += '凹んでるの？'
-  when /心折/
-    shinpai += '心折れてるの？'
-  when /(?:寂|淋)し/
-    shinpai += 'さびしいの？'
-  when /弱っ/
-    shinpai += '弱ってるの？'
-  when /つらい/
-    shinpai += 'つらくても、'
-  when /死にたい/
-    shinpai += '死なないで、'
-  when /お腹痛い/
-    shinpai += 'トイレ行って、'
-  when /(?:。。。|orz)/
-  when /テスト/
+  if status.reply? && status.user.screen_name != "kimataku_bot"
+    log.info('reply to @%s said : %s' % [status.user.screen_name, status.text])
+    message = '@%s ' % status.user.screen_name
+    message += reply_text(status.text)
+    log.info('dialog to @%s : %s' % [status.user.screen_name, message])
   else
-    next
+    shinpai = '@%s ' % status.user.screen_name
+    case status.text
+    when /https?:\/\//
+      next
+    when /疲(?!れ(?:様|さ(?:ま|ん)))/
+      shinpai += '疲れてるの？'
+    when /凹/
+      shinpai += '凹んでるの？'
+    when /心折/
+      shinpai += '心折れてるの？'
+    when /(?:寂|淋)し/
+      shinpai += 'さびしいの？'
+    when /弱っ/
+      shinpai += '弱ってるの？'
+    when /つらい/
+      shinpai += 'つらくても、'
+    when /死にたい/
+      shinpai += '死なないで、'
+    when /お腹痛い/
+      shinpai += 'トイレ行って、'
+    when /(?:。。。|orz)/
+    when /テスト/
+    else
+      next
+    end
+    hagemashi = rand > 0.05 ? 'げんきだして！' : 'まぁげんきだせやｗｗｗｗｗ'
+    message = shinpai + hagemashi
+    if rand < (status.user.screen_name == 'kimataku' ? 0.01 : 0.0001)
+      message = '@%s 社畜力がぐーんとアップしました' % status.user.screen_name
+    end
   end
 
-  hagemashi = rand > 0.05 ? 'げんきだして！' : 'まぁげんきだせやｗｗｗｗｗ'
-  message = shinpai + hagemashi
-  if rand < (status.user.screen_name == 'kimataku' ? 0.01 : 0.0001)
-    message = '@%s 社畜力がぐーんとアップしました' % status.user.screen_name
-  end
   begin
-    #tweet = rest.update("#{message}")
     tweet = rest.update("#{message}", in_reply_to_status_id: status.id)
     if tweet
       log.info('tweeted: %s' % tweet.text)
@@ -79,5 +83,23 @@ stream.on_timeline_status do |status|
   rescue => e
     log.error(e)
   end
+end
+
+def reply_text(text="")
+  apikey = ENV['DOCOMO_API_KEY']
+  uri = URI.parse("https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY=#{apikey}")
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+
+  body = {}
+  body['utt'] = text
+  body['t'] = 20
+
+  request = Net::HTTP::Post.new(uri.request_uri, {'Content-Type' =>'application/json'})
+  request.body = body.to_json
+  response = nil
+  resp = http.request(request)
+  response = JSON.parse(resp.body)
+  return response['utt']
 end
 stream.userstream
